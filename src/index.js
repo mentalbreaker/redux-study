13e81a528dde2aa85a7991e55e0826aa0ac285f8
lsa <pingu944@gmail.com>
2022-08-07 15:11:40 +0900

    리덕스 subscribe, refactoring

--- src/index.js
+++ src/index.js
@@ -12,15 +12,22 @@ const countModifier = (count = 0, action) => {
   } else if (action.type === "MINUS") {
     return count - 1;
   } else {
-    return count; // 리턴하는 결과는 곧 어플리케이션의 데이터가 된다.
+    return count; // reducer가 리턴하는 결과는 곧 어플리케이션의 데이터가 된다.
   }
 };
 
 const countStore = createStore(countModifier); //상태 및 데이터를 저장하는 곳. 괄호 안에 함수가 있어야 한다.
+// console.log(countStore.getState()); //countModifier의 리턴값을 출력한다.
 
-// countStore.dispatch({ type: "ADD" }); //(메시지를 보내는 것처럼)리듀서 함수로 들어가는 액션을 설정해줌.액션은 객체여야 한다.
+const whatIsChange = () => {
+  //이 function은 store에 변화가 있을 때마다 감지해서 불려진다.
+  //console.log(`countSotre.getState : ${countStore.getState()}`);
+  number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState()); //countModifier의 리턴값을 출력한다.
+countStore.subscribe(whatIsChange); //store에 변화가 있는 것을 알려줌
+
+// countStore.dispatch({ type: "ADD" }); //(메시지를 보내는 것처럼)리듀서 함수로 들어가는 액션을 설정해줌.액션은 type 키를 가진 객체여야 한다.
 
 function handleAdd() {
   countStore.dispatch({ type: "ADD" });
